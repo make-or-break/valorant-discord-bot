1dee8458c867985c7918eeef8ed33dab3e5bce94
lgoette <git@lasse-goette.de>
2022-06-23 19:21:36 +0200

    update onboarding

--- src/discord_bot/cogs/onboarding.py
+++ src/discord_bot/cogs/onboarding.py
@@ -1,11 +1,18 @@
+import imp
+from turtle import update
 import discord
 from discord.ext import commands
 from discord.ext import tasks
 
 import re
 
+from discord_bot.database.sql_statements import update_player
+
 from ..log_setup import logger
 from ..utils import utils as ut
+
+from ..valorant import *
+from ..database import *
 
 
 ### @package misc
@@ -29,15 +36,28 @@ class Onboarding(commands.Cog):
         message = "Welcome to the Server. Please send me your Valorant name and tagline in the following format: <name>#<tagline>";
         try:
             await member.send(message)
-            print(f"Onboarding DM sent to {member.name}, waiting for response.")
-
-            def check_response(res):
-                return re.search(re.compile(ur'\b(.{3,16}#.{3,5})\b'), res.content) and res.channel.type == discord.ChannelType.private and res.author == member #TODO: regex check außerhalb, wenn author und channel stimmt, um error message zu senden
-
-            msg = await self.client.wait_for('message', check=check_response)
-            print(f"Got valid response: {msg.content}")
         except:
             print('Could not DM user, closed DMs.')
+            exit
+
+        print(f"Onboarding DM sent to {member.name}, waiting for response.")
+
+        def check_response(res):
+            return res.channel.type == discord.ChannelType.private and res.author == member #TODO: regex check außerhalb, wenn author und channel stimmt, um error message zu senden
+
+        valid = False
+        while not valid:
+            response = await self.client.wait_for('message', check=check_response)
+            if (re.search(re.compile(ur'\b(.{3,16}#.{3,5})\b'), response.content)):
+                valid = True
+            else:
+                await member.send("Error: Please send a valid name and tagline in the following format: <name>#<tagline>")
+
+        player = response.split('#')
+        player_json = get_player_json(get_player_json(player[0], player[1]))
+        update_player("id", get_elo(player_json), get_rank(player_json), get_rank_tier(player_json), player[0], player[1])
+
+        await member.send("Thanks! Your Valorant Account is now connected!")
 
 def setup(bot):
     bot.add_cog(Onboarding(bot))

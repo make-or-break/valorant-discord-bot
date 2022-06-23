f5eebb948d963443c42bc468529e7a1a33dd414b
MayNiklas <info@niklas-steffen.de>
2022-06-23 15:30:55 +0200

    add sql stuff

---
+++ src/discord_bot/database/sql_scheme.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import sqlalchemy
+
+from sqlalchemy.orm import declarative_base, logger
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import event
+
+if not os.path.exists('data/'):
+    os.mkdir('data/')
+
+try:
+    engine = create_engine('sqlite:///data/players.db')
+    connection = engine.connect()
+    Base = declarative_base()
+
+    class Player(Base):
+        __tablename__ = 'players'
+        id = Column(Integer, primary_key=True)
+        elo = Column(Integer)
+        rank = Column(String)
+        rank_tier = Column(Integer)
+        tagline = Column(String)
+        username = Column(String)
+
+        def __repr__(self):
+            return f"id='{self.id}', username='{self.username}', tagline='{self.tagline}', elo='{self.elo}', rank='{self.rank}', rank_tier='{self.rank_tier}'"
+
+    Base.metadata.create_all(engine)
+
+    @event.listens_for(Base.metadata, 'after_create')
+    def receive_after_create(target, connection, tables, **kw):
+        """listen for the 'after_create' event"""
+        logger.info('A table was created' if tables else 'No table was created')
+        print('A table was created' if tables else 'No table was created')
+
+    def open_session() -> sqlalchemy.orm.Session:
+        """
+        :return: new active session
+        """
+        return sessionmaker(bind=engine)()
+
+except:
+    print("An exception while connecting to the DB has occurred")
+    sys.exit()
---
+++ src/discord_bot/database/sql_statements.py
@@ -0,0 +1,23 @@
+from sqlalchemy import select
+from sqlalchemy import update
+
+import sql_scheme as db
+
+
+def add_player(id, username, elo, rank, rank_tier, tagline, session=db.open_session()):
+    """
+    Add an entry to the players database
+    """
+    entry = db.Players(id=id, username=username, elo=elo,
+                       rank=rank, rank_tier=rank_tier, tagline=tagline)
+    print(
+        f'Add to database! id: {id} Username: {username} - elo: {elo} - rank: {rank} - rank_tier: {rank_tier} - tagline: {tagline}')
+    session.add(entry)
+    session.commit()
+
+
+def get_player(id, session=db.open_session()):
+    """
+    Get the player from the database
+    """
+    return session.query(db.Players).filter(db.Players.id == id).first()

5c1551bfe78d8fd164aa6d1430868a774248d347
MayNiklas <info@niklas-steffen.de>
2022-06-24 09:44:01 +0200

    add nix module

--- flake.nix
+++ flake.nix
@@ -8,6 +8,87 @@
 
   outputs = { self, nixpkgs, flake-utils, ... }:
 
+    {
+      nixosModules.default = self.nixosModules.valorant-discord-bot;
+      nixosModules.valorant-discord-bot = { lib, pkgs, config, ... }:
+        with lib;
+
+        let cfg = config.services.valorant-discord-bot;
+        in
+        {
+
+          options.services.valorant-discord-bot = {
+
+            enable = mkEnableOption "valorant-discord-bot";
+
+            dataDir = mkOption {
+              type = types.str;
+              default = "/var/lib/valorant-discord-bot";
+              description = ''
+                The directory where valorant-discord-bot stores its data files. If left as the default value this directory will automatically be created before the discord server starts, otherwise the sysadmin is responsible for ensuring the directory exists with appropriate ownership and permissions.
+              '';
+            };
+
+            envfile = mkOption {
+              type = types.str;
+              default = "/var/src/secrets/valorant-discord-bot/envfile";
+              description = ''
+                The location of the envfile containing secrets
+              '';
+            };
+
+            user = mkOption {
+              type = types.str;
+              default = "valorant-discord-bot";
+              description = "User account under which valorant-discord-bot runs.";
+            };
+
+            group = mkOption {
+              type = types.str;
+              default = "valorant-discord-bot";
+              description = "Group under which valorant-discord-bot runs.";
+            };
+
+          };
+
+          config = mkIf cfg.enable {
+
+            systemd.services.valorant-discord-bot = {
+              description = "A discord bot for our valorant group";
+              wantedBy = [ "multi-user.target" ];
+              serviceConfig = mkMerge [
+                {
+                  EnvironmentFile = [ cfg.envfile ];
+                  User = cfg.user;
+                  Group = cfg.group;
+                  WorkingDirectory = cfg.dataDir;
+                  ExecStart = "${self.packages."${pkgs.system}".valorant-discord-bot}/bin/valorant-discord-bot";
+                  Restart = "on-failure";
+                }
+                (mkIf (cfg.dataDir == "/var/lib/valorant-discord-bot") {
+                  StateDirectory = "valorant-discord-bot";
+                })
+              ];
+
+            };
+
+            users.users = mkIf (cfg.user == "valorant-discord-bot") {
+              valorant-discord-bot = {
+                isSystemUser = true;
+                group = cfg.group;
+                description = "valorant-discord-bot system user";
+              };
+            };
+
+            users.groups =
+              mkIf (cfg.group == "valorant-discord-bot") { valorant-discord-bot = { }; };
+
+          };
+          meta = { maintainers = with lib.maintainers; [ mayniklas ]; };
+        };
+    }
+
+    //
 
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};

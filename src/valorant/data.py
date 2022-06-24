9283f09271cbbea36f5dc12cbff6af1588af1dd7
lgoette <git@lasse-goette.de>
2022-06-24 22:43:23 +0200

    add rank data

--- src/valorant/data.py
+++ src/valorant/data.py
@@ -1,8 +1,139 @@
 # Valorant Rank information with rank_tier as key
 RANK_VALUE = {
-    0 : {
+    3 : {
         "name": "Iron I",
-        "elo": "",
-        "color": ""
+        "elo": "0",
+        "color": "#949494"
     },
+    4 : {
+        "name": "Iron II",
+        "elo": "100",
+        "color": "#949494"
+    },
+    5 : {
+        "name": "Iron III",
+        "elo": "200",
+        "color": "#949494"
+    },
+    6 : {
+        "name": "Bronze I",
+        "elo": "300",
+        "color": "#B49670"
+    },
+    7 : {
+        "name": "Bronze II",
+        "elo": "400",
+        "color": "#B49670"
+    },
+    8 : {
+        "name": "Bronze III",
+        "elo": "500",
+        "color": "#B49670"
+    },
+    9 : {
+        "name": "Silver I",
+        "elo": "600",
+        "color": "#E7EBEA"
+    },
+    10 : {
+        "name": "Silver II",
+        "elo": "700",
+        "color": "#E7EBEA"
+    },
+    11 : {
+        "name": "Silver III",
+        "elo": "800",
+        "color": "#E7EBEA"
+    },
+    12 : {
+        "name": "Gold I",
+        "elo": "900",
+        "color": "#ECD058"
+    },
+    13 : {
+        "name": "Gold II",
+        "elo": "1000",
+        "color": "#ECD058"
+    },
+    14 : {
+        "name": "Gold III",
+        "elo": "1100",
+        "color": "#ECD058"
+    },
+    15 : {
+        "name": "Platinum I",
+        "elo": "1200",
+        "color": "#3EB0C3"
+    },
+    16 : {
+        "name": "Platinum II",
+        "elo": "1300",
+        "color": "#3EB0C3"
+    },
+    17 : {
+        "name": "Platinum III",
+        "elo": "1400",
+        "color": "#3EB0C3"
+    },
+    18 : {
+        "name": "Diamond I",
+        "elo": "1500",
+        "color": "#A56DF2"
+    },
+    19: {
+        "name": "Diamond II",
+        "elo": "1600",
+        "color": "#A56DF2"
+    },
+    20: {
+        "name": "Diamond III",
+        "elo": "1700",
+        "color": "#A56DF2"
+    },
+    21: {
+        "name": "Ascendant I",
+        "elo": "1800",
+        "color": "#1E7644"
+    },
+    22: {
+        "name": "Ascendant II",
+        "elo": "1900",
+        "color": "#1E7644"
+    },
+    23: {
+        "name": "Ascendant III",
+        "elo": "2000",
+        "color": "#1E7644"
+    },
+    24: {
+        "name": "Immortal I",
+        "elo": "2100",
+        "color": "#BC314A"
+    },
+    25: {
+        "name": "Immortal II",
+        "elo": "2200",
+        "color": "#BC314A"
+    },
+    26: {
+        "name": "Immortal III",
+        "elo": "2300",
+        "color": "#BC314A"
+    },
+    27: {
+        "name": "Radiant",
+        "elo": "2400",
+        "color": "#FFFFB0"
+    }
 }
+
+# Color Codes:
+# Iron #C4C4C4 #949494 #646562
+# Bronze #CBB692 #B49670 #8D6F47
+# Silver #B9BEBC #E7EBEA #F5FCF7
+# Gold #F6E97C #EECF58 #ECD058
+# Platinum #53D2DE #3EB0C3 #3597A6
+# Diamond #F2D0F9 #C489F6 #A56DF2
+# Ascendant #7CFFBC #30A66F #1E7644
+# Immortal #E6776E #BC314A #B02639
+# Radiant #FEFFFF #E3D193 #FFFFB0

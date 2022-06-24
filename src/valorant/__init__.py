88c3c85fcd1d9b01964a5a23e6c79da3e37e3e19
Lasse Götte <git@lasse-goette.de>
2022-06-24 21:50:31 +0200

    zwischenstand valorant ranks

--- src/valorant/__init__.py
+++ src/valorant/__init__.py
@@ -1,1 +1,2 @@
 from .main import *
+from .data import RANK_VALUE
---
+++ src/valorant/data.py
@@ -0,0 +1,8 @@
+# Valorant Rank information with rank_tier as key
+RANK_VALUE = {
+    0 : {
+        "name": "Iron I",
+        "elo": "",
+        "color": ""
+    },
+}
